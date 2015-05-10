7e7ea594fae43c1c7682edc0dd715e7797d0b0b9
briecheese <brianna.kaufman+1@gmail.com>
2015-05-10 10:37:09 -0700

    Assignment 1

---
+++ plot2.R
@@ -0,0 +1,16 @@
+# read in our table
+power_consumption<-read.table("~/Downloads/household_power_consumption.txt", header=TRUE, sep=";", na.strings="?")
+power_consumption<-na.omit(power_consumption)
+# make sure that the Date row is formatted as a date & Time row as time
+power_consumption$Time<-strptime(paste(power_consumption$Date,power_consumption$Time),"%d/%m/%Y %H:%M:%S")
+power_consumption$Date<-as.Date(power_consumption$Date,"%d/%m/%Y")
+power_consumption<-subset(power_consumption,Date=="2007-02-02"|Date=="2007-02-01")
+
+
+##########
+# Plot 2 #
+##########
+
+png("plot2.png",width=480,height=480,units="px")
+plot(power_consumption$Time,power_consumption$Global_active_power,type="l",ylab="Global Active Power (kilowatts)",xlab="")
+dev.off()
